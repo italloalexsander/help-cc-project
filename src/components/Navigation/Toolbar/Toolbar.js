fd28ecbc4e5ebde8341116d7f6e7cbadd71c8be7
italloalexsander <itallo_codemaster@hotmail.com>
2021-05-19 21:29:49 -0300

    login "working", form started

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,17 @@
 import React from 'react' 
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
+import {NavLink} from 'react-router-dom'
 
 const toolbar = (props) =>(
     <header className={classes.Toolbar}>
         <div>MENU</div>
+        <NavLink to = {'/'} className = {classes.Link}>{props.name}
         <div><Logo type = 'toolbar'/></div>
+        </NavLink>
+        <NavLink to = {'/login'} className = {classes.Link}>{props.name}
+        <div><p className={link}>LOGIN</p></div>
+        </NavLink>
         <nav>
             ...
         </nav>
