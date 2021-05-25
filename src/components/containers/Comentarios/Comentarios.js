f1f9d6e0a5f49f9412a87eb9972edf74f54a2c98
italloalexsander <itallo_codemaster@hotmail.com>
2021-05-25 02:04:35 -0300

    comentários indo pra algum lugar, falta só mostrar

--- src/components/containers/Comentarios/Comentarios.js
+++ src/components/containers/Comentarios/Comentarios.js
@@ -1,17 +1,26 @@
+import axios from 'axios'
 import React, {Component} from 'react'
 
 class Comentarios extends Component{
 
     state ={
-
+        Dicas: true,
+        Sugestao: false,
+        Professor: false,
+        coment: []
     }
 
     componentDidMount(){
-        
+        axios.get(url).then(response=>{
+
+        })   
     }
 
 
     render(){
+
+
+
         return (
             <div>
                 <p>Comentários/Dicas/Sugestões:</p>
